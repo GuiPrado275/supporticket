be44c654fb6b7a3c2b87f48e3b91c5aa3a73330e
GuiPrado275 <guilherme.silva645@fatec.sp.gov.br>
2025-06-03 10:16:46 -0300

    Create ErrorResponse

---
+++ src/main/java/com/guilherme/supporticket/exceptions/ErrorResponse.java
@@ -0,0 +1,42 @@
+package com.guilherme.supporticket.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+
+    private final int status;
+    private final String message;
+    private String stackTrace;
+    private List<ValidationError> errors;
+
+    @Getter
+    @Setter
+    @RequiredArgsConstructor
+    public static class ValidationError {
+        private String field;
+        private String message;
+    }
+
+    public void addValidationError(String field, String message) {
+        if(Objects.isNull(errors)){
+            this.errors = new ArrayList<>(); // if object is null, create a new list
+        }
+        this.errors.add(new ValidationError(field, message));
+    }
+
+    public String toJson() {
+        return "{\"status\": " + getStatus() + ", " +
+                "\"message\": \"" + getMessage() + "\"}";
+    }
+}
